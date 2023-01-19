0d765862e0a3b4977410bba43f676c0ff197f57e
Albert Balbastre <albert@tipsterchat.com>
2023-01-19 11:25:53 +0100

    generate path filenames

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strings"
 )
+
+const ROOT_DIR = "docs"
+const PATHS_DIR = "paths"
+const DIR_SEP = "/"
 
 func main() {
 	filename := "./docs/doc.yaml"
@@ -21,7 +26,7 @@ func main() {
 		// if regex matches, then print the line
 		match, _ := regexp.MatchString(`^  /.*:$`, scanner.Text())
 		if match {
-			processPath(scanner.Text())
+			processPath(scanner)
 		}
 
 		if scanner.Text() == "components:" {
@@ -30,6 +35,36 @@ func main() {
 	}
 }
 
-func processPath(path) {
-	fmt.Println("processing", path)
+func processPath(scanner *bufio.Scanner) {
+	path := scanner.Text()
+	scanner.Scan()
+	method := scanner.Text()
+	scanner.Scan()
+	scanner.Scan()
+	scanner.Scan()
+	tagDir := scanner.Text()
+
+	filename := fmt.Sprintf(
+		"%s/%s/%s/%s_%s.yaml",
+		ROOT_DIR,
+		PATHS_DIR,
+		sanitize(tagDir),
+		sanitize(path),
+		sanitize(method))
+	fmt.Println("filename", filename)
+}
+
+func sanitize(s string) string {
+	s = strings.TrimSpace(s)
+	s = regexp.MustCompile(`-`).ReplaceAllString(s, "")
+	s = regexp.MustCompile(`:`).ReplaceAllString(s, "")
+	s = regexp.MustCompile(`/`).ReplaceAllString(s, "_")
+	s = regexp.MustCompile(`{`).ReplaceAllString(s, "-")
+	s = regexp.MustCompile(`}`).ReplaceAllString(s, "-")
+	s = regexp.MustCompile(`[A-Z]`).ReplaceAllStringFunc(s, func(r string) string {
+		return strings.ToLower(r)
+	})
+	s = strings.TrimSpace(s)
+
+	return s
 }
